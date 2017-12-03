86d6617fef505e27d2959f5a7207379cc10ad4ca
Andreas Galazis <agalazis@gmail.com>
2017-12-03 22:32:32 +0200

    fixed example immutable state assignment

--- src/examples/map-valid.ts
+++ src/examples/map-valid.ts
@@ -2,12 +2,12 @@ import { Map } from 'immutable';
 import * as logger from 'winston';
 import { Person } from './person';
 
-const a = Map<keyof Person, any>({
+let a = Map<keyof Person, any>({
 	isBusy: true,
 	somethingElse: 't',
 	surname: 'Smith',
 });
 // This following compiles but as you can see it's error prone
-a.set('name', true);
-a.set('isBusy', 'John');
+a = a.set('name', true);
+a = a.set('isBusy', 'John');
 logger.log('info', `The names is ${a.get('name')}. Now you get the issue..`);
