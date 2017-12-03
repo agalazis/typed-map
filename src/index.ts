c02390970fcb93b0742a46be9efc2182372e91a2
Andreas Galazis <agalazis@gmail.com>
2017-12-03 22:13:47 +0200

    removed unneeded types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export type TypedMap<T> = {
-  get: <K extends keyof T, L extends T>(k:K) => T[K];
-  set: <K extends keyof T, L extends T>(k:K, v:T[K]) => TypedMap<T>;
+  get: <K extends keyof T >(k:K) => T[K];
+  set: <K extends keyof T >(k:K, v:T[K]) => TypedMap<T>;
 }
